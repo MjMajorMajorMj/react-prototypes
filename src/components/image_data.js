c6367a03c09d7258670ee39dab692e2016473386
MjMajorMajorMj <techk8@gmail.com>
2018-09-12 15:07:09 -0700

    Finished styles-images

---
+++ src/components/image_data.js
@@ -0,0 +1,45 @@
+import hmmmOne from './images/hmmone.jpg';
+import hmmmTwo from './images/hmmtwo.jpg';
+import hmmmThree from './images/hmmthree.jpg';
+import hmmmFour from './images/hmmfour.jpg';
+import hmmmFive from './images/hmmfive.jpg';
+
+export default [
+    {
+        src: hmmmOne,
+        style: {
+            left: '5%',
+            transform: 'rotate(-10deg)'
+        }
+    },
+    {
+        src: hmmmTwo,
+        style: {
+            left: '40%',
+            transform: 'rotate(6deg)'
+        }
+    },
+    {
+        src: hmmmThree,
+        style: {
+            right: '15%',
+            transform: 'rotate(-9deg)'
+        }
+    },
+    {
+        src: hmmmFour,
+        style: {
+            left: '8%',
+            top: '28%',
+            transform: 'rotate(8deg)'
+        }
+    },
+    {
+        src: hmmmFive,
+        style: {
+            left: '37%',
+            top: '25%',
+            transform: 'rotate(-12deg)'
+        }
+    }
+];
---
+++ src/components/scrapbook.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './scrapbook.css';
+import ScrapbookImage from './scrapbook_images';
+import imageData from './image_data';
+
+export default props => {
+    const images = imageData.map((img, index) => {
+        return <ScrapbookImage key={index} about={img} />
+    });
+    return (
+        <div className="scrapbook-container">
+            {images}
+        </div>
+    )
+}
